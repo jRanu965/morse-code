e621238a20869befc7a2f7aeef669b0bb156aabb
jRanu965 <jasnoorr965@deltalearns.ca>
2026-04-13 08:54:05 -0700

    Add pseudocode

--- main.py
+++ main.py
@@ -4,3 +4,19 @@ def main():
     print("Morse Code Program Starting...")
 
 main() 
+
+# Morse Code Encryptor / Decryptor
+# PSEUDOCODE:
+# 1. Create Morse dictionary
+# 2. Create reverse dictionary
+# 3. Format input
+# 4. Encrypt function
+# 5. Decrypt function
+# 6. Ask user for choice
+# 7. Process input
+# # 8. Display result
+
+def main():
+    print("Morse Code Program Starting...")
+
+main() 
